c776f2d40b3a20f4c05bcd0f81d96138be33bf44
Thon Becker <thon.becker@daveramsey.com>
2022-12-01 18:28:06 -0600

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
-const { javascript } = require("projen");
-const project = new javascript.NodeProject({
-  defaultReleaseBranch: "main",
-  name: "advent-of-code",
+const { typescript } = require('projen');
+const project = new typescript.TypeScriptProject({
+  defaultReleaseBranch: 'main',
+  name: 'advent-of-code',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
